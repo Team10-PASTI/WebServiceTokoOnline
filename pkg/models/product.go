04168334553268e66efcf4709ac1fc5aeb28d951
Wordyka Nainggolan <wordykanainggolan1212@gmail.com>
2022-05-13 15:37:43 +0700

    test

--- pkg/models/product.go
+++ pkg/models/product.go
@@ -10,7 +10,6 @@ var db *gorm.DB
 // Membuat sebuah struct unutk mencreate tabel Produk dan tipe data setiap kolomnya
 type Produk struct {
 	gorm.Model
-	ID      int `json:"id"`
 	Nama     string `gorm:""json:"nama"`
 	Kategori string `json:"kategori"`
 	Harga    uint   `json:"harga"`
@@ -43,14 +42,14 @@ func GetAllProduk() []Produk {
 // fungsi untuk mengambil data table sesuai dengan id yang di request
 func GetProdukbyId(id int64) (*Produk, *gorm.DB) {
 	var getProduk Produk
-	db := db.Where("id=?", id).Find(&getProduk)
+	db := db.Where("ID=?", id).Find(&getProduk)
 	return &getProduk, db
 }
 
 // fungsi untuk menghapus data table sesuai dengan id yang di request
 func DeleteProduk(id int64) Produk {
 	var produk Produk
-	db.Where("id=?", id).Delete(produk)
+	db.Where("ID=?", id).Delete(produk)
 	return produk
 
 }
